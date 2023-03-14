d74c7f096db6161ae4c3d2865c3a25d8d96ba2c8
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-14 09:11:31 +0900

    import static java

--- src/ch07/Ex07_06.java
+++ src/ch07/Ex07_06.java
@@ -1,10 +1,17 @@
 package ch07;
+import static java.lang.System.out;
+import static java.lang.Math.*;
 
 public class Ex07_06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//System.out.println(Math.random());
+		out.println(random());
+		
+		//System.out.println("Math.PI: " + Math.PI);
+		out.println("Math.PI: " + PI);
+		
 	}
 
 }
