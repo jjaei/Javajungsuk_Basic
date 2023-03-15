180bd339ac6de828e2fb926a98aacac6aca607bb
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-16 08:33:31 +0900

    고의로 오류 발생시키기

--- src/ch08/Ex08_07.java
+++ src/ch08/Ex08_07.java
@@ -4,7 +4,7 @@ public class Ex08_07 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		throw new Exception();  // 오류를 고의로 발생시킨다.
+	//	throw new Exception();  // 오류를 고의로 발생시킨다.
 
 	}
 
