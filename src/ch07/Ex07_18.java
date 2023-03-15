a79d5dbd445039da95cd7343f802aa59136d0a10
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-15 10:28:59 +0900

    ActionListener

---
+++ src/ch07/Ex07_18.java
@@ -0,0 +1,21 @@
+package ch07;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Ex07_18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Button b = new Button("Start");
+		b.addActionListener(new EventHandler());
+	}
+
+}
+
+class EventHandler implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("ActionEvent occurred!!!");
+	}
+}
