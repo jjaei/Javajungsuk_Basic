8f7ac408ac68bb57b4185639b0b65072e2260c6e
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-15 10:31:03 +0900

    ActionListener 익명 클래스

---
+++ src/ch07/Ex07_19.java
@@ -0,0 +1,17 @@
+package ch07;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Ex07_19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Button b = new Button("Start");
+		b.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("ActionEvent occurred!!!");
+				}
+			} // 익명 클래스의 끝
+		);
+	} // main의 끝
+} 
