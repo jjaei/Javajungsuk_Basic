b3d82afd1da70167066d4f010b13fcbddeb211bc
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-16 09:12:19 +0900

    equals

---
+++ src/ch09/Ex09_01.java
@@ -0,0 +1,26 @@
+package ch09;
+
+public class Ex09_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Value v1 = new Value(10);
+		Value v2 = new Value(10);
+		
+		if(v1.equals(v2)) {
+			System.out.println("v1과 v2는 같습니다.");
+		} else {
+			System.out.println("v1과 v2는 다릅니다.");
+		} // true
+	}
+
+}
+
+class Value{ 
+	int value;
+	
+	Value(int value) {
+		this.value = value;
+	}
+}
