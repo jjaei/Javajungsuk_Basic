ec83d9cc2c73f53dec095d4839dc914b2d59bc4f
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-20 10:14:57 +0900

    String 예제

---
+++ src/ch09/Ex09_07.java
@@ -0,0 +1,19 @@
+package ch09;
+
+public class Ex09_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+		
+		System.out.println(s1 == s2); // true
+		System.out.println(s1.equals(s2)); // true
+		System.out.println(s3 == s4); // false
+		
+	}
+
+}
