25a764f2db2117e27c3681613da99b0fe266772a
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-27 18:50:53 +0900

    싱글스레드 - JOptionPane.showInputDialog로 입력받기

--- src/ch13/Ex13_04.java
+++ src/ch13/Ex13_04.java
@@ -10,10 +10,12 @@ public class Ex13_04 {
 		String s = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
 		System.out.println("입력하신 값은 " + s + "입니다.");
 		
-		for(int i=0; i<10; i++) {
+		for(int i=10; i>0; i--) {
 			System.out.println(i);
 			try {
 				Thread.sleep(1000);   // 1초간 시간을 지연시킨다.
+				// 입력을 받고나서 카운트다운이 시작됨.
+				// 입력받는데 기다리는 시간이 낭비됨.
 			} catch(Exception e) {}
 		}
 	}
