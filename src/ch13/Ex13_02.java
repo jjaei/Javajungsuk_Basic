dbcccd9e5c9742c1249b0137273242aaa914c569
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-27 18:39:48 +0900

    싱글스레드

--- src/ch13/Ex13_02.java
+++ src/ch13/Ex13_02.java
@@ -5,6 +5,18 @@ public class Ex13_02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		long startTime = System.currentTimeMillis();
+		for(int i=0; i<300; i++) {
+			System.out.printf("%s", new String("-"));
+		}
+		
+		System.out.print("소요시간1: " + (System.currentTimeMillis() - startTime));
+		
+		for(int i=0; i<300; i++) {
+			System.out.printf("%s", new String("/"));
+		}
+		
+		System.out.print("소요시간2: " + (System.currentTimeMillis() - startTime));
 	}
 
 }
