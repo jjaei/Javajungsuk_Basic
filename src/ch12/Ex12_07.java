5a13dacd07fa957c0237973888f30defc4cdb415
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-27 09:59:42 +0900

    애너테이션-Override

--- src/ch12/Ex12_07.java
+++ src/ch12/Ex12_07.java
@@ -5,6 +5,6 @@ class Parent {
 }
 
 class Child extends Parent {
-	@Override
-	void parentmethod() {}  // 조상 메서드의 이름을 잘못 적은 경우 애너테이션이 에러를 알려줌.
+//	@Override
+//	void parentmethod() {}  // 조상 메서드의 이름을 잘못 적은 경우 애너테이션이 에러를 알려줌.
 }
