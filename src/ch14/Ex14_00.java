96a21cc9d01b4a433992adb12a8047882e719048
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-29 11:39:01 +0900

    람다식->함수(메서드)를 간단한 식으로 표현

--- src/ch14/Ex14_00.java
+++ src/ch14/Ex14_00.java
@@ -12,15 +12,25 @@ public class Ex14_00 {
 		// 매개변수가 하나일 경우는 괄호 생략 가능하다. (타입이 있으면 생략 불가능)
 		
 //		Object obj = (a, b) -> a > b ? a : b   // 람다식. 익명객체
-		Object obj = new Object() {
-			int max(int a, int b) {
-				return a > b ? a : b;
-			}
-		};
+//		MyFunction2 f = new MyFunction2() {
+//			public int max(int a, int b) {
+//				return a > b ? a : b;
+//			}
+//		};
+		
+		// 람다식(익명 객체)을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
+		MyFunction2 f = (a, b) -> a > b ? a : b;
 			
 //		int value = obj.max(3, 5); // object 타입에는 max라는 메서드가 없어서 에러.
 		// 형변환 할 만한 것도 없음. 함수형 인터페이스가 필요함.
+		
+		int value = f.max(3,  5);
+		System.out.println("value= " + value);
 	}
 
 }
 
+@FunctionalInterface
+interface MyFunction2 {  // 함수형 인터페이스는 단 하나의 추상 메서드만 가져야 함.
+	public abstract int max(int a, int b);
+}
