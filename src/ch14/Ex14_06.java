519aa906001b6f1cc730537e274c8ebaabd020ee
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-30 09:49:24 +0900

    스트림의 중간연산2

--- src/ch14/Ex14_06.java
+++ src/ch14/Ex14_06.java
@@ -22,6 +22,8 @@ public class Ex14_06 {
 		
 		fileStream.map(File::getName)   // Stream<File> -> Stream<String>
 		.filter(s -> s.indexOf('.')!=-1)   // 확장자가 없는 것은 제외한다.
+		.peek(s -> System.out.printf("filename=%s%n",s))
+		// 스트림을 소모하지 않고 중간 작업결과를 볼 수 있다.
 		.map(s -> s.substring(s.indexOf('.')+1))   // 확장자만 추출한다.
 		.map(String::toUpperCase)   // 모두 대문자로 변환한다.
 		.distinct()     // 중복을 제거한다.
